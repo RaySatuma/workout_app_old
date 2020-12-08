76591e6b93055f2727e37ebc8e39232d24a96962
EC2 Default User <ec2-user@ip-172-31-41-88.ec2.internal>
2020-12-08 12:00:34 +0000

    Add signup, error-explanation, flash-messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,26 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = 'ユーザーを登録しました。'
+      redirect_to root_path
+    else
+      flash[:danger] = 'ユーザー登録が正常にできませんでした。'
+      render :new
+    end
   end
+
+  private
+    
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_comfirmation)
+    end  
 end
+
+
